f94ae10040cf33c2eeda450ec3277fc59beb4ea8
jaspercurry <jaspercurry@gmail.com>
2016-07-04 22:06:02 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,38 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+
+  data.each do |property, v|
+    v.each do |value, v1|
+        v1.each do |name|
+            new_hash[name]= {}
+        end
+    end
+  end
+ 
+  data.each do |property, v|
+    v.each do |value, v1|
+        v1.each do |name|
+            new_hash[name][property] = []
+        end
+    end
+  end
+
+    data.each do |property, v|
+    v.each do |value, v1|
+        v1.each do |name|
+            new_hash[name][property] << value.to_s
+        end
+    end
+  end
+  new_hash
 end
+
+#nyc_pigeon_organizer(pigeon_data)
+#new_hash[name] = ""
+
+#new_hash[name]= {}
+
+
+# new_hash[name][k] = []
+# new_hash[name][k] << k1
+# new_hash[name] = {k => [k1]}
