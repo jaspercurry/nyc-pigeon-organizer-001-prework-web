72a1e0a6e7da674b68bf80a2d9ae05e58a1772ee
jaspercurry <jaspercurry@gmail.com>
2016-07-04 22:26:18 -0400

    Done.

--- organizer_original.rb
+++ organizer_original.rb
@@ -1,26 +1,6 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
 
-  data.each do |property, result|
-    result.each do |value, names|
-        names.each do |name|
-            new_hash[name] ||= {}
-            new_hash[name][property] ||= []
-            new_hash[name][property] << value.to_s
-        end
-    end
-  end
-  new_hash
- end
-
- # http://stackoverflow.com/questions/1389081/what-does-the-operator-stands-for-in-ruby?lq=1
-
- #old answer:
-=begin
-
-def nyc_pigeon_organizer(data)
-  new_hash = {}
-
   data.each do |property, v|
     v.each do |value, v1|
         v1.each do |name|
@@ -46,5 +26,3 @@ def nyc_pigeon_organizer(data)
   end
   new_hash
 end
-
-=end
